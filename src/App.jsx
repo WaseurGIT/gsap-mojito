ba60c254b4ae7c490a7bdab57dda2bc4078fd9cb
WaseurGIT <waseurrahman31@gmail.com>
2025-12-23 13:13:06 +0600

    implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // step: 1 --> import plugins and register them
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-500 font-semibold">GSAP Mojito</h1>
-    </div>
+    <main>
+      <Navbar></Navbar>
+    </main>
   );
 }
 
